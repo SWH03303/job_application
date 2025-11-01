e23f6252ddc21cfcca199c254dec112c05759ea6
SWH03403 <106224482@student.swin.edu.au>
2025-11-02 01:14:59 +0700

    feat: impl renderers for components & page content

---
+++ src/core/render.php
@@ -0,0 +1,17 @@
+<?php
+function render(string $component) {
+	global $opts;
+	require COMPONENTS_DIR . "/$component.php";
+}
+
+function render_page(array|callable|string $content, array $g_opts = []) {
+	global $opts;
+	$opts = $g_opts;
+	$opts['title'] = $opts['title'] ?? 'Unnamed page';
+
+	render('wraps/top');
+	if (is_string($content)) { echo "$content"; }
+	elseif (is_array($content)) { foreach ($content as $c) { render($c); }}
+	else { $content(); }
+	render('wraps/bottom');
+}
--- src/init.php
+++ src/init.php
@@ -3,6 +3,7 @@ declare(strict_types = 1);
 
 const DEFAULT_ROUTE = 'root';
 const MIGRATIONS_TABLE = '__migrations';
+const COMPONENTS_DIR = __DIR__ . '/parts';
 define('DATABASE_URL', dirname(__DIR__) . '/db.sqlite');
 define('MIGRATIONS_DIR', dirname(__DIR__) . '/migrations');
 
