025279bd917e0db51e5d9d6db4562167599813f5
SWH03403 <106224482@student.swin.edu.au>
2025-11-12 22:11:58 +0700

    feat: validate session and log user in after signing up

--- src/routes/session/signup.php
+++ src/routes/session/signup.php
@@ -1,18 +1,30 @@
 <?php
 $dname = null;
 $email = null;
-$on_post = function() use (&$dname, &$email): array {
+$errors = [];
+if (Request::is_post()) {
 	$dname = Request::param('dname');
 	$email = Request::param('email');
-	$pass1 = Request::param('pass1');
-	$pass2 = Request::param('pass2');
+	$pass1 = Request::param('pass1', false);
+	$pass2 = Request::param('pass2', false);
 
-	return [];
-};
-$errors = Request::is_post()? $on_post() : [];
+	if (strlen($dname) > 20) { $errors[] = 'Display name is too long'; }
+	if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) { $errors[] = 'Email is invalid'; }
+	if (strlen($email) > 30) { $errors[] = 'Email is too long'; }
+	if (strlen($pass1) < 30) { $errors[] = 'Password is too short'; }
+	if ($pass1 != $pass2) { $errors[] = 'Password repetition does not match'; }
+	if (!empty($errors)) { goto end_post; }
 
-render_page(['forms/signup'], [
+	User::register($email, $pass1, $dname);
+	User::login($email, $pass1);
+	Router::redirect(Router::DEFAULT);
+}
+end_post:
+
+render_page(['forms/new_user'], [
 	'title' => 'Sign up',
+	'style' => 'signup',
 	'dname' => $dname,
 	'email' => $email,
+	'errors' => $errors,
 ]);
